3118c08b0f54cdd4ab5d10b1ca05b90e159ddddd
Chikara Saito <nagulog@gmail.com>
2019-12-01 22:43:23 +0900

    ADD 指定したWebサイトでのみextensionが機能する

--- background.js
+++ background.js
@@ -1,16 +1,10 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ color: "#3aa757" }, function () {
+    console.log("The color is green.");
+  });
+  chrome.storage.sync.set({ site: [] }, function () {
+    console.log("site settins.")
+  })
 
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-          conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostContains: '.com'},
-          })
-          ],
-              actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-      });
-
+  chrome.declarativeContent.onPageChanged.removeRules(undefined);
 });
--- options.js
+++ options.js
@@ -1,15 +1,48 @@
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-        let button = document.createElement('button');
-        button.style.backgroundColor = item;
-        button.addEventListener('click', function() {
-            chrome.storage.sync.set({color: item}, function() {
-                console.log('color is ' + item);
-            })
+function applySiteList() {
+    chrome.storage.sync.get("site", function (data) {
+        // get site list
+        let targetSiteList = [];
+        for (let sitePattern of data.site) {
+            targetSiteList.push(new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostContains: sitePattern }
+            }));
+        }
+
+        // add rules
+        chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+            chrome.declarativeContent.onPageChanged.addRules([
+                {
+                    conditions: targetSiteList,
+                    actions: [new chrome.declarativeContent.ShowPageAction()]
+                }
+            ]);
         });
-        page.appendChild(button);
-    }
-}
-constructOptions(kButtonColors);
+    });
+};
+
+function insertSiteList(targetElement) {
+    chrome.storage.sync.get("site", function (data) {
+        for (let sitePattern of data.site) {
+            targetElement.appendChild(document.createElement("li"));
+            let input_text = document.createElement("input");
+            input_text.setAttribute("type", "text");
+            input_text.setAttribute('value', sitePattern);
+            input_text.setAttribute('readonly', 1)
+            targetElement.appendChild(input_text);
+        }
+    });
+};
+let siteList = document.getElementById("siteList");
+insertSiteList(siteList);
+applySiteList();
+
+function addSiteList(newSite) {
+    chrome.storage.sync.get("site", function (data) {
+        chrome.storage.sync.set({ site: [...data.site, newSite] });
+    });
+};
+let submitButton = document.getElementById("addSite");
+submitButton.addEventListener('click', function () {
+    const newSite = document.getElementById("newSite").value;
+    addSiteList(newSite);
+});
